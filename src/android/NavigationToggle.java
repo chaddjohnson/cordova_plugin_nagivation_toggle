eae8697c6de5f1cb7ce671e51c0f21b5f35b19ec
Chad Johnson <chad.d.johnson@gmail.com>
2014-10-05 23:27:16 -0500

    Added missing PluginResult import.

--- src/android/NavigationToggle.java
+++ src/android/NavigationToggle.java
@@ -2,6 +2,7 @@ package com.bshmedia.NavigationToggle;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
